adf637ff7ea82df0acc1f1889aaa88d83a5fc29c
niklaas12 <niklas.arnold.na@gmail.com>
2025-03-26 10:29:49 +0100

    maybe now

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,18 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.isc.meik.app',
   appName: 'meikApp',
-  webDir: 'dist/meik-app/browser'
+  webDir: 'dist/meik-app/browser',
+  plugins: {
+    CapacitorHttp: {
+      enabled: true,
+    },
+  },
+  android: {
+    allowMixedContent: true
+  },
+  server: {
+    allowNavigation: ['*.industrieschule.com'], // Füge hier deinen Domainnamen hinzu
+  },
 };
 
 export default config;
